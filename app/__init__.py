dc33aed0ce4b5cacef3ba3a53e11dc9877bb2eac
Muhammadsiddiqxon <coder7946@gmail.com>
2022-07-12 17:46:12 +0500

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_ckeditor import CKEditor
+from config import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .main import main
+    from .admin import admin
+
+    app.register_blueprint(main, url_prefix='/')
+    app.register_blueprint(admin, url_prefix='/admin')
+
+    return app
